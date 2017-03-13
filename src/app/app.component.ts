970d520934f52b358e9305863718b96aad1e920c
Sergio Gutiérrez <sergut18@gmail.com>
2017-03-12 20:17:18 -0500

    added Chronometer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
 export class AppComponent {
-  title = 'app works!';
+  title = 'Mi primera aplicación Angular 2!';
+  public hora : number;
+  public minuto : number;
+  public segundos : number;
+
+  public horaLapso : number = 0;
+  public minutoLapso : number = 0;
+  public segundosLapso : number = 0;
+
+  public collection : Array < any > = [];
+  public contador : any;
+
+  constructor() {
+    this.hora = 0;
+    this.minuto = 0;
+    this.segundos = 0;
+  }
+
+  start = () => {
+    if (this.contador == undefined) {
+      this.contador = setInterval(() => {
+        this.segundos++;
+        if (this.segundos === 60) {
+          this.segundos = 0;
+          this.minuto++;
+          if (this.minuto === 60) {
+            this.minuto = 0;
+            this.hora++;
+            if (this.hora === 20) {
+              this.hora = 0;
+            }
+          }
+        }
+      }, 1);
+    }
+  }
+
+  lapso = () => {
+    let obj : any = {};
+    obj.hora = this.hora;
+    obj.minutos = this.minuto;
+    obj.segundos = this.segundos;
+
+    this
+      .collection
+      .push(obj);
+  }
+
+  stop = () => {
+    clearInterval(this.contador);
+    this.hora = 0;
+    this.minuto = 0;
+    this.segundos = 0;
+    this.contador = null;
+  }
 }
